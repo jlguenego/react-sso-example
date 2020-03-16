8f234ef209bc918890dfc1cab34d9bf9234e9f7a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-16 17:10:16 +0100

    logo

--- front/src/layout/Header/Header.tsx
+++ front/src/layout/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.scss';
+import logo from '../../assets/logo.svg';
 
 export default () => (
   <header>
     <a href="#">
+      <img src={logo} alt="SSO Logo" />
       <span>React SSO Example</span>
     </a>
   </header>
