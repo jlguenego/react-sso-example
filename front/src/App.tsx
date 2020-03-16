8541f42db74cc6d78e24091349604fe8239d34d3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-16 17:59:53 +0100

    add Connection

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,10 +3,12 @@ import './App.scss';
 import Header from './layout/Header/Header';
 import Footer from './layout/Footer/Footer';
 import Main from './layout/Main/Main';
+import Connection from './layout/Connection/Connection';
 
 export default () => (
   <div className="App">
     <Header />
+    <Connection />
     <Main />
     <Footer />
   </div>
---
+++ front/src/layout/Connection/Connection.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Connection.scss';
+
+export default () => (
+  <section className="Connection">
+    <button>Connect</button>
+  </section>
+);
