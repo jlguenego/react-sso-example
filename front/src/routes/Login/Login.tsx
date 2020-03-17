68c3ac833a18d9044075d5fdafcddd155334429c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-17 10:26:21 +0100

    added connection

--- front/src/routes/Login/Login.tsx
+++ front/src/routes/Login/Login.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import './Login.scss';
+import { useHistory } from 'react-router-dom';
 
-export default () => (
-  <main className="Login">
-    <section>
-      <button className="primary">Connect with SSO</button>
-      <div className="or"></div>
-      <form>
-        <label>
-          <div>Login</div>
-          <input type="text" />
-        </label>
-        <label>
-          <div>Password</div>
-          <input type="password" />
-        </label>
-        <button>Connect</button>
-      </form>
-    </section>
-  </main>
-);
+export default () => {
+  let history = useHistory();
+
+  async function connectWithSSO() {
+    console.log('connectWithSSO');
+    const response = await window.fetch('/ws/connect-with-sso');
+    const json = await response.json();
+    console.log('json: ', json);
+    history.push('/');
+  }
+
+  return (
+    <main className="Login">
+      <section>
+        <button className="primary" onClick={connectWithSSO}>
+          Connect with SSO
+        </button>
+        <div className="or"></div>
+        <form>
+          <label>
+            <div>Login</div>
+            <input type="text" />
+          </label>
+          <label>
+            <div>Password</div>
+            <input type="password" />
+          </label>
+          <button>Connect</button>
+        </form>
+      </section>
+    </main>
+  );
+};
