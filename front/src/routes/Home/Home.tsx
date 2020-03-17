43fbdefd7193456d0ee4f6a9875c6798065ebe86
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-17 09:20:06 +0100

    state

--- front/src/routes/Home/Home.tsx
+++ front/src/routes/Home/Home.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.scss';
 import Connection from '../../widgets/Connection/Connection';
 
-export default (props: any) => (
-  <main className="Home">
-    <h1>Welcome !</h1>
-    <button className="primary">Look at my secret...</button>
-    <Connection />
-  </main>
-);
+export default class Home extends Component<{}, { secret: string }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { secret: 'xxx' };
+  }
+
+  showSecret() {
+    this.setState({
+      secret: 'hello this is my secret'
+    });
+  }
+
+  render() {
+    return (
+      <main className="Home">
+        <h1>Welcome !</h1>
+        <Connection />
+        <button className="primary secret" onClick={this.showSecret.bind(this)}>
+          Look at my secret...
+        </button>
+        <span>{this.state.secret}</span>
+      </main>
+    );
+  }
+}
